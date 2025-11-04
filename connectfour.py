41b3bb9564320531cce7f986ffd969414d6ae573
eremyta <72329895+eremyta@users.noreply.github.com>
2025-11-04 17:03:49 -0300

    Update
    Inplementação das classes jogo e jogador
    Métodos tabuleiro_cheio adicionado
    Métodos da classe jogo e jogador implementados

--- connectfour.py
+++ connectfour.py
@@ -1,3 +1,5 @@
+import random
+
 class Tabuleiro():
     def __init__(self):
         self.NUM_LINHAS = 6
@@ -15,8 +17,16 @@ class Tabuleiro():
         
         return -1
     
+    def tabuleiro_cheio(self):
+        linha_topo = self.tabuleiro[0]
+        for i in linha_topo:
+            if i == self.PECA_VAZIA:
+                return False    
+        return True
+
     
-    def showTabuleiro(self):
+    
+    def show_tabuleiro(self):
         print("\n  0 1 2 3 4 5 6")
         print(" +-------------+")
         
@@ -30,12 +40,44 @@ class Tabuleiro():
         self.tabuleiro[linha][coluna] = peca
 
     
-tab1 = Tabuleiro()
-tab1.showTabuleiro()
 
 class Jogador():
     def __init__(self, nome, peca, tipo='humano'):
+        self.nome = nome
+        self.peca = peca
+        self.tipo = tipo
+    
+    def obter_jogada(self, tabuleiro):
         pass
+
+class Jogo():
+    def __init__(self):
+        self.tab = Tabuleiro()
+        self.jogador1 = None
+        self.jogador2 = None
+        self.quemJoga = None
+    
+    def startJogo(self):
+        print("Bem-vindo ao Connect Four (Quatro em Linha)!")
+        
+        while True:
+            try:
+                modo = int(input("Escolha o modo de jogo (1: Humano x Humano, 2: Humano x Computador): "))
+                modos_aceitos = {
+                    1: ('Humano X', 'humano', 'Humano O', 'humano'),
+                    2: ('Humano X', 'humano', 'Computador O', 'computador')}
+                if modo not in modos_aceitos:
+                    raise ValueError('Opção inválida')
+                print(f'Iniciando partida {modos_aceitos[modo]}')
+                setup = modos_aceitos[modo]
+                self.jogador1 = Jogador(setup[0], 'X', setup[1])
+                self.jogador2 = Jogador(setup[2], 'O', setup[3])
+                print(f"\nPartida iniciada: {self.jogador1.nome} ('X') vs {self.jogador2.nome} ('O')")
+                break
+            except ValueError as e:
+                print(f'Erro {e}. Tente Novamente')
+            
+
 
         
     
